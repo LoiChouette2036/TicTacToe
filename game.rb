73313bd4affcd089dc5595572b40beda39cf12da
LoiChouette2036 <tristanbellanger10@gmail.com>
2024-08-01 06:32:58 +0900

    deleted an empty file and rename an important one

---
+++ game.rb
@@ -0,0 +1,48 @@
+class Game
+    def initialize
+        @board = Board.new
+        @player1 = Player.new("Player 1", "X")
+        @player2 = Player.new("Player 2", "O")
+    end
+
+    def start 
+        @board.display 
+        puts "Let's the game begin !!!"
+    end
+
+    def who_is_playing?
+        print "who's playing ?"
+        user_input = gets.chomp.to_i
+
+        if user_input == 1 
+            @current_player = @player1
+        else
+            @current_player = @player2
+        end
+    end
+    
+    def choose_position        
+        print "Choose between from 1 to 9, where you want to tick your marker"
+        @position_wanted = gets.chomp.to_i
+        puts "You entered the number: #{@position_wanted}"
+    end
+
+    def write_down_user_input
+        loop do
+            choose_position
+            if @board[@position_wanted-1] == ""
+                @board[@position_wanted-1] = @current_player.marker
+                @board.display
+                return true
+            else
+                puts "you can not play there, retry an other place please"
+            end
+        end
+    end
+
+    def check_winner 
+    end
+    
+
+
+end
